0917da7abf8912f217cfd68afd2146150d78e913
HerrAndersson <anderssonjonas94@gmail.com>
2015-11-23 16:59:16 +0100

    Added variables

--- nibbles.s
+++ nibbles.s
@@ -1,20 +1,30 @@
+.section .data
+
+wormChar:	.long	111
+appleChar:	.long	36
+nrOfApples: .long	0
+maxLength:	.long	0
+
 .section .text
 .globl start_game
 start_game:
-	pop		%eax
-	pop		%eax
+
+	popl	%eax
+	movl	%eax, nrOfApples
+	popl	%eax
+	movl	%eax, maxLength
 	
 	call	nib_init
 	
-label:
-	pushl	$104
+label: 
+
+	pushl	$nrOfApples
 	pushl	$10
 	pushl	$10
 	
 	call	nib_put_scr
 	addl	$12, %esp
 	jmp		label
-	
 	
 	call	nib_end
 
