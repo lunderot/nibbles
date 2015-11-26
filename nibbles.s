0bdc5d378b2558db441e0c1c8ae13674597ed5f2
HerrAndersson <anderssonjonas94@gmail.com>
2015-11-26 17:14:16 +0100

    Tried to fix assembly code. Not working

--- nibbles.s
+++ nibbles.s
@@ -1,24 +1,44 @@
+.section .bss
+var:	.long	0
+
 .section .data
+
+
+.set CONSTANT, 20
+
 
 wormChar:	.long	111
 appleChar:	.long	36
 nrOfApples: .long	0
 maxLength:	.long	0
 
+currentLength:	.long	0 #set to len
+direction:		.long	0
+hit:			.long	0
+done:			.long	0
+input:			.long	0xffffffff
+
+
 .section .text
 .globl start_game
 start_game:
 
 	popl	%eax
-	movl	%eax, nrOfApples
+	movl	%eax, var
+	
+	
+	
+	
+	
 	popl	%eax
-	movl	%eax, maxLength
+	
 	
 	call	nib_init
 	
 label: 
 
-	pushl	$nrOfApples
+	movl	$var, %eax
+	pushl	(%eax)
 	pushl	$10
 	pushl	$10
 	
